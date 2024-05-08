5f3dd7b024c704416628917c282ce59821af8f62
Shinyura <tlsdbfk_@naver.com>
2024-05-08 14:41:57 +0900

    Algorithm

---
+++ 이것이코딩테스트다/Chapter17/Q40-2.py
@@ -0,0 +1,51 @@
+import heapq
+
+n, m = map(int, input().split())
+
+array = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    array[a].append((b, 1))
+    array[b].append((a, 1))
+
+INF = int(1e9)
+distance = [INF] * (n+1)
+
+start = 1
+
+def dijkstra(start):
+    q = []
+
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if dist > distance[now]:
+            continue
+
+        for i in array[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+print(distance)
+
+max_node = 0
+max_distance = 0
+result = []
+
+for i in range(1, n+1):
+    if max_distance < distance[i]:
+        max_node = i
+        max_distance = distance[i]
+        result = [max_node]
+    elif max_distance == distance[i]:
+        result.append(max_node)
+    
+print(max_node, max_distance, len(result))
