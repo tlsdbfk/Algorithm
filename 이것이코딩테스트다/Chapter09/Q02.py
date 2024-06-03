84995025c5597a5f678b634ec8ecd12e6cf183bd
Shinyura <tlsdbfk_@naver.com>
2024-06-03 13:39:51 +0900

    Algorithm

--- 이것이코딩테스트다/Chapter09/Q02.py
+++ 이것이코딩테스트다/Chapter09/Q02.py
@@ -1,39 +1,29 @@
-import heapq
-
 INF = int(1e9)
 
-n, m, start = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-distance = [INF] * (n + 1)
+n, m = map(int, input().split())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
 
 for _ in range(m):
-    x, y, z = map(int, input().split())
-    graph[x].append((y, z))
+    a, b = map(int, input().split())
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-        for i in graph[now]:
-            cost = dist + i[1]
+x, k = map(int, input().split())
 
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-dijkstra(start)
+distance = graph[1][k] + graph[k][x]
 
-count = 0
-max_distance = 0
-
-for d in distance:
-    if d != INF:
-        count += 1
-        max_distance = max(max_distance, d)
-
-print(count-1, max_distance)
+if distance >= INF:
+    print(-1)
+else:
+    print(distance)
